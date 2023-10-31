6fa4cb1ad7de43ce9dc511b9b2e5ff83ff2f7755
quill_nichols <leplep00100@gmail.com>
2023-10-30 22:20:58 -0400

    wonderful

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${eventName} gift!`);
+}
+
+// Example usage:
+const namesArray = ["Guadalupe", "Ollie", "Aki"];
+const event = "surprise";
+
+const thankYouMessages = writeCards(namesArray, event);
+console.log(thankYouMessages);
+
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
+
+
+console.log(thankYouMessages);
+
+countDown(5);
